df638f679af886a8a7158f8119aa733bb627f88b
Raul Torres <jr.torsa@gmail.com>
2021-08-01 23:57:58 -0600

    added a href

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ import { burgerMenu } from "./dom/burgerMenu.js"
 import { digitalClock, alarm } from "./dom/clock.js"
 
 document.addEventListener('DOMContentLoaded', e => {
-    burgerMenu(".hamburger", "#myModal", ".section-content")
+    burgerMenu(".hamburger", "#myModal", ".section-content a")
     digitalClock("#clock", "#activar-reloj", "#desactivar-reloj")
 })
